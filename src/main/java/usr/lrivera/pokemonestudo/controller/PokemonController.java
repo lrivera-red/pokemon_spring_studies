2d04ee6ed6e3d71a443b0c672ca2c32f545d5890
lrivera-red <lgsrivera21@gmail.com>
2022-01-26 21:02:23 -0300

    Mudanças dias 26.01

--- src/main/java/usr/lrivera/pokemonestudo/controller/PokemonController.java
+++ src/main/java/usr/lrivera/pokemonestudo/controller/PokemonController.java
@@ -1,14 +1,22 @@
 package usr.lrivera.pokemonestudo.controller;
 
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientException;
 import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
 import usr.lrivera.pokemonestudo.controller.services.PokemonIPTrackingService;
 import usr.lrivera.pokemonestudo.dto.PokemonDTO;
 import usr.lrivera.pokemonestudo.dto.PokemonDetalhadoDTO;
@@ -19,28 +27,38 @@ import usr.lrivera.pokemonestudo.repository.PokemonRepository;
 //import usr.lrivera.pokemonestudo.repository.PokemonRepository;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.net.URI;
-import java.util.ArrayList;
+import java.time.Duration;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/pokemon")
 public class PokemonController {
+    HttpClient httpClient = HttpClient.create()
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+            .responseTimeout(Duration.ofMillis(5000))
+            .doOnConnected(conn ->
+                    conn.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS))
+                            .addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS)));
     @Autowired
     private PokemonIPTrackingService pokemonIPTrackingService;
     @Autowired
     private PokemonRepository pokemonRepository;
     private static final Logger log = LoggerFactory.getLogger(PokemonController.class);
+    private static final Logger jsonlog = LoggerFactory.getLogger("jsonLogger");
     @GetMapping
     public List<PokemonDTO> listaCompleta(@RequestHeader HttpHeaders headers) {
-        headers.getOrigin();
+        WebClient webClient = WebClient.builder().baseUrl("https://webhook.site/d30dec1b-2f5c-4f09-98a8-eee7e74794ba").defaultHeaders().clientConnector(new ReactorClientHttpConnector(httpClient)).build();
+       String response = webClient.get().retrieve().bodyToMono(String.class).block();
+       log.info(response);
+       headers.getOrigin();
         String idOptional = headers.getFirst("id");
         if (idOptional!=null) {
-            log.info("Request geral");
+            log.info("Request geral aaaaaaaaaaaaaaaa");
+            jsonlog.info("Request aaaaaaaaaaaaaaa");
             return PokemonDTO.converterPokemonFromHeader(pokemonRepository.findById(Long.valueOf(idOptional)).get());
         }
         List<Pokemon> pokemons = pokemonRepository.findAll();
